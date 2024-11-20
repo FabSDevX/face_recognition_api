ca1dcb8ac1bfb1ff42a2d8de26fdeb5f11ad774e
Fabricio Porras Morera <faporras05@hotmail.com>
2024-11-20 11:28:32 -0600

    Docker

--- install_cmake.sh
+++ install_cmake.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-apt-get update && apt-get install -y cmake
+
+# Asegúrate de tener las actualizaciones más recientes
+apt-get update
+
+# Descarga e instala CMake
+apt-get install -y cmake
+
+# Verifica la instalación de CMake
+cmake --version
